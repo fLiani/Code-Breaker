6b2d9f73114a95cd1763f104b4a90a2539b73db7
FabPizza <98763483+FabPizza@users.noreply.github.com>
2022-03-08 13:23:41 +0000

    Frame check

--- gui.java
+++ gui.java
@@ -1,4 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class gui
 {
-    
+    private JFrame frame;
+    private JPanel mainPanel;
+    private JPanel colourMenu;
+    private JPanel score;
+
+    private JButton empty;
+    private JButton whiteTick;
+    private JButton blackTick;
+    private JButton r;
+    private JButton o;
+    private JButton y;
+    private JButton g;
+    private JButton b;
+    private JButton i;
+    private JButton v;
+
+
+    public gui()
+    {
+        frame = new JFrame("Code Breaker! - By Fabrizio Liani");
+        mainPanel = new JPanel();
+        
+        colourMenu = new JPanel();
+
+        frame.setContentPane(mainPanel);
+
+        frame.setSize(240,800);
+
+        frame.setVisible(true);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
