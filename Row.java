93eda5a67d1a519c8242467de3ce8a87b0230712
FabPizza <98763483+FabPizza@users.noreply.github.com>
2022-03-12 20:12:30 +0000

    progress

--- Row.java
+++ Row.java
@@ -1,18 +1,17 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
-public class Row
+public class Row implements ActionListener
 {
-    JPanel panel = new JPanel();
-    JPanel panel2 = new JPanel();
-    JPanel container = new JPanel();
-    JPanel link = new JPanel();
+    private JPanel panel = new JPanel();
+    private JPanel panel2 = new JPanel();
+    private JPanel container = new JPanel();
+    private JPanel link = new JPanel();
 
     private Picture empty = new Picture("Empty.png");
 
     private JButton[] button = new JButton[4];
-
-    private int bSize = 65;
 
     public Row()
     {
@@ -20,14 +19,14 @@ public class Row
         {
             button[i] = new JButton(empty);
             panel.add(button[i]);
-            button[i].setPreferredSize(new Dimension(bSize, bSize));
+            button[i].setPreferredSize(new Dimension(62, 62));
         }
 
         for(int j = 0; j < 4; j++)
         {
             button[j] = new JButton(empty);
             panel2.add(button[j]);
-            button[j].setPreferredSize(new Dimension(bSize, 50));
+            button[j].setPreferredSize(new Dimension(90, 50));
         }
 
         panel2.setLayout(new GridLayout(2, 2));
@@ -42,5 +41,17 @@ public class Row
         panel2.setBackground(Color.GRAY);
         container.setBackground(Color.GRAY);
         link.setBackground(Color.GRAY);
+
+
+    }
+
+    public JPanel getLink()
+    {
+        return link;
+    }
+
+    public void actionPerformed(ActionEvent event)
+    {
+
     }
 }
