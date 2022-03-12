9eeb9d4b36111d1faecfcd64d9b0c61c74fce96a
FabPizza <98763483+FabPizza@users.noreply.github.com>
2022-03-12 18:22:36 +0000

    progress

--- Row.java
+++ Row.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 public class Row
 {
     JPanel panel = new JPanel();
+    JPanel panel2 = new JPanel();
+    JPanel container = new JPanel();
+    JPanel link = new JPanel();
 
     private Picture empty = new Picture("Empty.png");
 
@@ -19,10 +22,25 @@ public class Row
             panel.add(button[i]);
             button[i].setPreferredSize(new Dimension(bSize, bSize));
         }
-    }
 
-    public JPanel getRow()
-    {
-        return panel;
+        for(int j = 0; j < 4; j++)
+        {
+            button[j] = new JButton(empty);
+            panel2.add(button[j]);
+            button[j].setPreferredSize(new Dimension(bSize, bSize));
+        }
+
+        panel2.setLayout(new GridLayout(2, 2));
+        container.setLayout(new BorderLayout());
+
+        link.add("West", panel);
+        link.add("East", panel2);
+
+        container.add("Center", link);
+
+        panel.setBackground(Color.GRAY);
+        panel2.setBackground(Color.GRAY);
+        container.setBackground(Color.GRAY);
+        link.setBackground(Color.GRAY);
     }
 }
