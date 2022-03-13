7f6960aa7ade17b8c86ec52e2467a44d10efdde7
FabPizza <98763483+FabPizza@users.noreply.github.com>
2022-03-13 16:45:40 +0000

    progress

--- Guess.java
+++ Guess.java
@@ -1,18 +1,28 @@
-import java.util.Random;
+import javax.swing.*;
+import java.awt.*;
+
 
 public class Guess
 {
+    private Picture empty = new Picture("Empty.png");
+
+    private JButton[] button = new JButton[4];
+
+    private JPanel panel = new JPanel();
 
     public Guess()
     {
         for(int i = 0; i < 4; i++)
         {
-            Random g = new Random();
-
-            int randCode = g.nextInt(7);
-
-            System.out.println(randCode);
+            button[i] = new JButton(empty);
+            panel.add(button[i]);
+            button[i].setPreferredSize(new Dimension(62, 62));
         }
     }
 
+    public JPanel getPanel()
+    {
+        return panel;
+    }
+ 
 }
