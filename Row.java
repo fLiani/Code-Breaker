d5d227dd1a3535269312334a6967e91b0c2536e6
FabPizza <98763483+FabPizza@users.noreply.github.com>
2022-03-15 12:53:28 +0000

    task2

--- Row.java
+++ Row.java
@@ -1,36 +1,47 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.text.AttributeSet.ColorAttribute;
+
 import java.awt.event.*;
 
 public class Row implements ActionListener
 {
+    private JPanel panel = new JPanel();
     private JPanel panel2 = new JPanel();
     private JPanel container = new JPanel();
     private JPanel link = new JPanel();
 
     private Picture empty = new Picture("Empty.png");
 
-    private JButton[] button = new JButton[4];
+    JButton[] rowButton = new JButton[5];
 
-    private Guess newGuess = new Guess();
+    private JButton[] gridButton = new JButton[4];
 
     public Row()
     {
-        for(int j = 0; j < 4; j++)
+        for(int j = 0; j < gridButton.length; j++)
         {
-            button[j] = new JButton(empty);
-            panel2.add(button[j]);
-            button[j].setPreferredSize(new Dimension(90, 50));
+            gridButton[j] = new JButton(empty);
+            panel2.add(gridButton[j]);
+            gridButton[j].setPreferredSize(new Dimension(90, 50));
+        }
+
+        for(int i = 0; i < rowButton.length - 1; i++)
+        {
+            rowButton[i] = new JButton(empty);
+            panel.add(rowButton[i]);
+            rowButton[i].setPreferredSize(new Dimension(62, 62));
         }
 
         panel2.setLayout(new GridLayout(2, 2));
         container.setLayout(new BorderLayout());
 
-        link.add("West", newGuess.getPanel());
+        link.add("West", panel);
         link.add("East", panel2);
 
         container.add("Center", link);
 
+        panel.setBackground(Color.GRAY);
         panel2.setBackground(Color.GRAY);
         container.setBackground(Color.GRAY);
         link.setBackground(Color.GRAY);
