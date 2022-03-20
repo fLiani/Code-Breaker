47315d32aeb1a5f0203df02c01adca1e866f0abe
FabPizza <98763483+FabPizza@users.noreply.github.com>
2022-03-20 17:55:53 +0000

    finished

--- gameBoard.java
+++ gameBoard.java
@@ -1,7 +1,13 @@
+/**
+ * Code Breaker Game By Fabrizio Liani
+ * Main class for Code Breaker
+ * @param game The starter of the program
+ */
+
 public class gameBoard
 {
     public static void main(String[] args)
     {
-        Gui empty = new Gui();
+        Gui game = new Gui();
     }
 }
